8fb28a80a7ca455d2e2d911ab361f5c8a43287e8
Mario Diana <software@mariodiana.com>
2021-06-09 23:07:21 -0400

    Add gain control in UI

--- Demo/ViewController.m
+++ Demo/ViewController.m
@@ -6,17 +6,46 @@
 //
 
 #import "ViewController.h"
+#import "DemoViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic, weak) IBOutlet UILabel* gainValue;
+@property (nonatomic, weak) IBOutlet UISlider* gainSlider;
+@property (nonatomic, weak) DemoViewController* demo;
 @end
+
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
 
+
+- (IBAction)updateGain:(id)sender
+{
+    UISlider* slider = sender;
+    self.gainValue.text = [NSString stringWithFormat:@"Gain: %.1f", [slider value]];
+}
+
+
+- (IBAction)captureGain:(id)sender
+{
+    [self updateGain:sender];
+    UISlider* slider = sender;
+    Float32 gain = (Float32)[slider value];
+    [[self demo] setGain:gain];
+}
+
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Grab a handle from the storyboard.
+    if ([@"DemoViewControllerSegue" isEqualToString:[segue identifier]]) {
+        self.demo = [segue destinationViewController];
+    }
+}
 
 @end
